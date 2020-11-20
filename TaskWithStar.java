c7c6a500c11eb08b2cbaf57528327175eff7d48f
Kristina <filippovvakristina@gmail.com>
2020-11-20 20:29:55 +0200

    add loop and method

--- TaskWithStar.java
+++ TaskWithStar.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 public class TaskWithStar {
     public static void main (String [] args) {
         Scanner in = new Scanner(System.in);
-        int num, res, a, b, c;
-        num = in.nextInt();
-        a = num %10;
-        num /= 10;
-        b = num %10;
-        num /= 10;
-        c = num %10;
-        num/= 10;
-        res= num + a + b + c;
-        System.out.println(res);
-
-
-
+        System.out.println("Enter number: ");
+        int x = readInput(in);
+        numberSum(x);
     }
-    
+
+    public static int readInput (Scanner in) {   
+        int x = in.nextInt();
+        return x;
+    }
+
+    public static int numberSum (int x) {
+        int sum=0;
+        while (x>0) {
+            sum +=x%10;
+            x/=10;
+        }
+        System.out.println(sum);
+        return sum;
+    }    
 }
